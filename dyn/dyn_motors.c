ba4871a936af983b2ab035acbb14064adbe28cf6
Richard <rsberrocal@gmail.com>
2020-04-18 18:02:24 +0200

    motores

--- dyn/dyn_motors.c
+++ dyn/dyn_motors.c
@@ -3,12 +3,27 @@
 //
 
 #include "dyn_motors.h"
+#include "dyn_frames.h"
+
 void wheelMode(void){
-    //Creating instr
+    //Creating instruction
     byte id = 0xFE;//BROADCASTING
     byte instruccion = 0x03; //WRITE_DATA
-    byte paramsLength = 5;
-    byte parameters
+    byte paramsLength = 5; //Number of data to write +1
+    byte parameters[16];
 
+    parameters[0] = 0x06;//Start at CW_ANGLE_LIMIT_L
+
+    parameters[1] = 0; // CW_ANGLE_LIMIT_L = 0
+    parameters[2] = 0; // CW_ANGLE_LIMIT_H = 0
+    parameters[3] = 0; // CCW_ANGLE_LIMIT_L = 0
+    parameters[4] = 0; // CCW_ANGLE_LIMIT_H = 0
+
+    TxPacket(id,paramsLength,instruccion,parameters);
+}
+
+void moveWheel(byte ID, bool rotation,unsigned int speed){
+
+    
 
 }
