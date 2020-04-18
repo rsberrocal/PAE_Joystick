3a93f22296dd96504393f141adb7932380a5a9ce
Richard <rsberrocal@gmail.com>
2020-04-18 16:09:10 +0200

    Added coded

--- dyn/dyn_frames.c
+++ dyn/dyn_frames.c
@@ -10,6 +10,7 @@
 #include "dyn_frames.h"
 
 #ifndef __MSP432P401R__
+
 #include "../hal_dyn_uart/hal_dyn_uart_emu.h"
 #include "../dyn_test/fake_msp.h"
 
@@ -33,58 +34,70 @@
 
 //TxPacket()  3 par�metres: ID del Dynamixel, Mida dels par�metres, Instruction byte. torna la mida del "Return packet"
 byte TxPacket(byte bID, byte bParameterLength, byte bInstruction,
-		const byte * Parametros) {
-	byte bCount, bCheckSum, bPacketLength;
-	byte TxBuffer[32];
-	f_Sentit_Dades_Tx();  //El pin P3.0 (DIRECTION_PORT) el posem a 1 (Transmetre)
-	TxBuffer[0] = 0xff;    //Primers 2 bytes que indiquen inici de trama FF, FF.
-	TxBuffer[1] = 0xff;
-	TxBuffer[2] = bID;         //ID del m�dul al que volem enviar el missatge
-	TxBuffer[3] = bParameterLength + 2; //Length(Parameter,Instruction,Checksum)
-	TxBuffer[4] = bInstruction;    //Instrucci� que enviem al M�dul
+              const byte *Parametros) {
+    byte bCount, bCheckSum, bPacketLength;
+    byte TxBuffer[32];
+    f_Sentit_Dades_Tx();  //El pin P3.0 (DIRECTION_PORT) el posem a 1 (Transmetre)
+    TxBuffer[0] = 0xff;    //Primers 2 bytes que indiquen inici de trama FF, FF.
+    TxBuffer[1] = 0xff;
+    TxBuffer[2] = bID;         //ID del m�dul al que volem enviar el missatge
+    TxBuffer[3] = bParameterLength + 2; //Length(Parameter,Instruction,Checksum)
+    TxBuffer[4] = bInstruction;    //Instrucci� que enviem al M�dul
 
-	//TODO: La instrucci� no ha de poder modificar les primeres 5 instruccions
+    //TODO: La instrucci� no ha de poder modificar les primeres 5 instruccions Ya esta hecho
 
-	for (bCount = 0; bCount < bParameterLength; bCount++) //Comencem a generar la trama que hem d�enviar
-			{
-		TxBuffer[bCount + 5] = Parametros[bCount];
-	}
-	bCheckSum = 0;
-	bPacketLength = bParameterLength + 4 + 2;
-	for (bCount = 2; bCount < bPacketLength - 1; bCount++) //C�lcul del checksum
-			{
-		bCheckSum += TxBuffer[bCount];
-	}
-	TxBuffer[bCount] = ~bCheckSum;         //Escriu el Checksum (complement a 1)
-	for (bCount = 0; bCount < bPacketLength; bCount++) //Aquest bucle �s el que envia la trama al M�dul Robot
-			{
-		f_TxUAC2(TxBuffer[bCount]);
-	}
-	while ((UCA2STATW & UCBUSY)) {
-	};   //Espera fins que s�ha transm�s el �ltim byte
-	f_Sentit_Dades_Rx(); //Posem la l�nia de dades en Rx perqu� el m�dul Dynamixel envia resposta
-	return (bPacketLength);
+    for (bCount = 0; bCount < bParameterLength; bCount++) //Comencem a generar la trama que hem d�enviar
+    {
+        TxBuffer[bCount + 5] = Parametros[bCount];
+    }
+    bCheckSum = 0;
+    bPacketLength = bParameterLength + 4 + 2;
+    for (bCount = 2; bCount < bPacketLength - 1; bCount++) //C�lcul del checksum
+    {
+        bCheckSum += TxBuffer[bCount];
+    }
+    TxBuffer[bCount] = ~bCheckSum;         //Escriu el Checksum (complement a 1)
+    for (bCount = 0; bCount < bPacketLength; bCount++) //Aquest bucle �s el que envia la trama al M�dul Robot
+    {
+        f_TxUAC2(TxBuffer[bCount]);
+    }
+    while ((UCA2STATW & UCBUSY)) {
+    };   //Espera fins que s�ha transm�s el �ltim byte
+    f_Sentit_Dades_Rx(); //Posem la l�nia de dades en Rx perqu� el m�dul Dynamixel envia resposta
+    return (bPacketLength);
 }
 
 struct RxReturn RxPacket(void) {
-	struct RxReturn respuesta;
-	byte bCount;
+    struct RxReturn respuesta;
+    byte bCount;
 
-	respuesta.time_out = false;
-	respuesta.idx = 0;
-	f_Sentit_Dades_Rx();   //Ponemos la linea half duplex en Rx
-	f_Activa_Timer_TimeOut();
-	for (bCount = 0; bCount < 4; bCount++) {
-		f_rx_uart_byte(&respuesta);
-	} //fin del for
-	if (!respuesta.time_out) {
-		for (bCount = 0; bCount < respuesta.StatusPacket[3]; bCount++) {
-			f_rx_uart_byte(&respuesta);
-		} //fin del for
-	}
-	//TODO: Decode packet and verify checksum
+    respuesta.time_out = false;
+    respuesta.idx = 0;
+    f_Sentit_Dades_Rx();   //Ponemos la linea half duplex en Rx
+    f_Activa_Timer_TimeOut();
+    for (bCount = 0; bCount < 4; bCount++) {
+        f_rx_uart_byte(&respuesta);
+    } //fin del for
+    if (!respuesta.time_out) {
+        for (bCount = 0; bCount < respuesta.StatusPacket[3]; bCount++) {
+            f_rx_uart_byte(&respuesta);
+        } //fin del for
+    }
+    //TODO: Decode packet and verify checksum
+    byte bChecksum = 0;
+    if (!respuesta.time_out) {
+        byte bLenght = respuesta.StatusPacket[3] + 4;
+        for (bCount = 2; bCount < bLenght - 1; bCount++) {
+            bChecksum += respuesta.StatusPacket[bCount];
+        }
+        bChecksum = ~bChecksum;
+        if (respuesta.StatusPacket[bLenght - 1] != bChecksum) { // comprovem el checksum
+            respuesta.tx_err = true; // error si no coincideix
+        }
+    }
+    //si que hay hacerlo
 
-	return respuesta;
+    return respuesta;
 }
 
 
@@ -101,9 +114,9 @@ struct RxReturn RxPacket(void) {
  * @return Returns a RxReturn struct with the information of the reply
  */
 struct RxReturn RxTxPacket(byte bID, byte bParameterLength, byte bInstruction,
-		const byte * Parametros) {
-	struct RxReturn respuesta;
-	TxPacket(bID, bParameterLength, bInstruction, Parametros);
-	respuesta = RxPacket();
-	return respuesta;
+                           const byte *Parametros) {
+    struct RxReturn respuesta;
+    TxPacket(bID, bParameterLength, bInstruction, Parametros);
+    respuesta = RxPacket();
+    return respuesta;
 }
