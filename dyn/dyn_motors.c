53c906068b1cc783ba438b7c2dcb2c27a1cb85f6
Richard <rsberrocal@gmail.com>
2020-04-25 17:59:45 +0200

    Motors hechos

--- dyn/dyn_motors.c
+++ dyn/dyn_motors.c
@@ -5,9 +5,9 @@
 #include "dyn_motors.h"
 #include "dyn_frames.h"
 
-void wheelMode(void){
+void wheelMode(byte idMotor) {
     //Creating instruction
-    byte id = 0xFE;//BROADCASTING
+    byte id = idMotor;//Not broadcasting for the emulator so setting the two motors manually
     byte instruccion = 0x03; //WRITE_DATA
     byte paramsLength = 5; //Number of data to write +1
     byte parameters[16];
@@ -19,11 +19,84 @@ void wheelMode(void){
     parameters[3] = 0; // CCW_ANGLE_LIMIT_L = 0
     parameters[4] = 0; // CCW_ANGLE_LIMIT_H = 0
 
-    TxPacket(id,paramsLength,instruccion,parameters);
+    TxPacket(id, paramsLength, instruccion, parameters);
 }
 
-void moveWheel(byte ID, bool rotation,unsigned int speed){
+//moveTo true -> right; false -> left
+void moveWheel(byte ID, bool moveTo, unsigned int speed) {
 
-    
+    struct RxReturn returnPacket;
+    byte speed_H, speed_L;
+    speed_L = speed; //speed always will be les than 1024
+
+    //To rotate
+    if (moveTo) { //rotate to right means CW 4, MOVING SPEED H 0100
+        speed_H = (speed >> 8) + 4;
+    } else { //rotate to right means CCW 0, MOVING SPEED H 0000
+        speed_H = speed >> 8;
+    }
+
+    byte instr = WRITE_DATA;//Write data
+    byte paramsLength = 3;
+    byte parameters[16];
+
+    parameters[0] = MOV_SPEED_L; //Start at mov speed low
+
+    parameters[1] = speed_L; //write low
+    parameters[2] = speed_H; //write high
+
+    TxPacket(ID, paramsLength, instr, parameters);
+    returnPacket = RxPacket();
+}
+
+void stop(void) {
+    moveWheel(RIGHT_WHEEL, false, 0);
+    moveWheel(LEFT_WHEEL, false, 0);
+}
+
+void turnLeft(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, true, speed);
+        moveWheel(LEFT_WHEEL, true, 0);
+    }
+}
+
+void turnOnItselfLeft(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, true, speed);
+        moveWheel(LEFT_WHEEL, true, speed);
+    }
+}
+
+void turnRight(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, false, 0);
+        moveWheel(LEFT_WHEEL, false, speed);
+    }
+}
+
+
+void turnOnItselfRight(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, false, speed);
+        moveWheel(LEFT_WHEEL, false, speed);
+    }
+}
+
+void forward(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, true, speed);
+        moveWheel(LEFT_WHEEL, false, speed);
+    }
+}
+
+void backward(unsigned int speed) {
+    if (speed < MAX_SPEED) {
+        moveWheel(RIGHT_WHEEL, false, speed);
+        moveWheel(LEFT_WHEEL, true, speed);
+    }
+}
+
+void endlessTurn(unsigned int speed) {
 
 }
--- dyn/dyn_motors.h
+++ dyn/dyn_motors.h
@@ -4,25 +4,44 @@
 
 #ifndef JOYSTICK_DYN_MOTORS_H
 #define JOYSTICK_DYN_MOTORS_H
+
 #include <stdint.h>
 #include <stdbool.h>
+//Wheels
+#define RIGHT_WHEEL 2
+#define LEFT_WHEEL 3
+
+//Instruccions
+#define WRITE_DATA 0x03
+#define READ_DATA 0x02
+
+//Directions
+#define CW_ANGLE_LIMIT_L 0x06
+#define CW_ANGLE_LIMIT_H 0x07
+#define CCW_ANGLE_LIMIT_L 0x08
+#define CCW_ANGLE_LIMIT_H 0x09
+
+//Speed
+#define MOV_SPEED_L 0x20
+#define MOV_SPEED_H 0x21
+#define M_LED 0x19
+#define MAX_SPEED 1024
+
 typedef uint8_t byte;
 
-void wheelMode(void);//to rotate wheels
+void wheelMode(byte idMotor);//to set wheels
 
-void moveWheel(byte ID, bool rotation,unsigned int speed);
+//moveTo -> true Right; false Left
+void moveWheel(byte ID, bool moveTo, unsigned int speed);
 
 void stop(void);
 
 void turnLeft(unsigned int speed);
 
-void turnLeftD(unsigned int degree);
-
 void turnOnItselfLeft(unsigned int speed);
 
 void turnRight(unsigned int speed);
 
-void turnRightD(unsigned int degree);
 
 void turnOnItselfRight(unsigned int speed);
 
@@ -30,7 +49,6 @@ void forward(unsigned int speed);
 
 void backward(unsigned int speed);
 
-
-
+void endlessTurn(unsigned int speed);
 
 #endif //JOYSTICK_DYN_MOTORS_H
